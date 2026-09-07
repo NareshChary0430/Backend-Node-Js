fc744fdcdea0ad3268aafd8c6fa19faa15c9eb77
NareshChary0430 <nareshchari300@gmail..com>
2026-09-07 11:30:08 +0530

    implemented multer

--- 07-filehandling-multer/src/config/multer.js
+++ 07-filehandling-multer/src/config/multer.js
@@ -1,6 +1,8 @@
 const multer = require('multer');
 const path = require('path');
 
+
+// for local storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
@@ -9,6 +11,11 @@ const storage = multer.diskStorage({
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
+
+
+// for server
+
+// const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage });
 
